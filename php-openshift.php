54600fc353cf241a6ae39c97e0afe846bbabf79f
aazamora-fuentezuelas <115076722+aazamora-fuentezuelas@users.noreply.github.com>
2023-02-28 21:56:24 +0100

    Add files via upload

---
+++ php-openshift.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Prueba PHP OpenShift</title>
+	<link rel="stylesheet" href="tablas.css">
+</head>
+<body>
+    <header>
+        <nav class="menu">
+     		 <div class="menu-item">
+				<p>Hola, visitante</p>
+        	</div>
+        	<div class="menu-item right">
+                <?php echo "Andalucía, ". date("d/m/Y"); ?>
+        	</div>
+		</nav>
+    </header>
+		<h1>Prueba de visualización en OpenShift</h1>
+	<footer>
+        <div class="footer-links">
+            <a href="index.php">Prueba de archivo PHP para su despliegue en OpenShift.</a>
+        </div>
+        <div class="logout">
+        	<a href="#">Alfonso Zamora. I.E.S. Las Fuentezuelas</a>
+        </div>
+	</footer>
+</body>
+</html>
+		
+
